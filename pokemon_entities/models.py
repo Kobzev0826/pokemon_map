8e072f7e60ca69e688e864c0be4ff3cf4c09ea01
Vladimir Kobzev <93667972+Kobzev0826@users.noreply.github.com>
2023-01-30 12:50:21 +0300

    delete comments in models.py

--- pokemon_entities/models.py
+++ pokemon_entities/models.py
@@ -1,6 +1,5 @@
 from django.db import models  # noqa F401
 
-# your models here
 class Pokemon(models.Model):
     title = models.CharField('Название покемона', max_length=200)
     photo = models.ImageField('Фото покемона', upload_to='pokemons', null=True, blank=True)
